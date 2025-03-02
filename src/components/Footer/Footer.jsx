7d6f35ad3c977857753961237c4b0e3ca2034ce2
harshit <harshit@dev.familyone.io>
2025-03-02 12:18:11 +0530

    add name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Mohan Nager, Uttar Pradesh</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+91 7703801967</p>
                 </div>
               </div>
             </div>
